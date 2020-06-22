3a979a2b4b215fe6babf585c7ecb385833762117
benruwel <benruwel21@gmail.com>
2020-06-22 12:18:24 +0300

    freshened up the UI

--- src/app/directives/repo-highlighter.directive.ts
+++ src/app/directives/repo-highlighter.directive.ts
@@ -1,25 +1,35 @@
-import { UserComponent } from './../views/user/user.component';
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
-import { Users } from "../user-class/user.model";
+import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appRepoHighlighter]'
 })
 export class RepoHighlighterDirective {
 
-  user : UserComponent;
 
   constructor(private elem : ElementRef, private renderer : Renderer2) {
 
     // thirenderer.setStyle('elem.nativeElement', 'color', 'red')
     
-    let repoCount = this.user.repoCount;
-    console.log(repoCount);
-    if ( repoCount == 10 ) {
-      this.elem.nativeElement.style.color = "red";
+    // let repoCount = this.user.repoCount;
+    // console.log(repoCount);
+    // if ( repoCount == 10 ) {
+    //   this.elem.nativeElement.style.color = "red";
       
-    }
+    // }
   }
+
+  @HostListener ('mouseenter') onMouseEnter() {
+    this.highlight('green');
+  }
+
+  @HostListener ('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+
+  private highlight(color : string) {
+    this.elem.nativeElement.style.color = color;
+  }
+
 
   
 
--- src/app/views/repos/repos.component.ts
+++ src/app/views/repos/repos.component.ts
@@ -1,4 +1,3 @@
-import { RepoSearch } from './../../repo-class/repo-search';
 import { RepoRequestService } from './../../services/repo-request.service';
 import { Component, OnInit } from '@angular/core';
 import { RepoSearchModel } from 'src/app/repo-class/repo-search.model';
